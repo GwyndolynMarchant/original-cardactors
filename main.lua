44e127dc9bebe5f9a674dcce43f3c347aaf7e40e
Juniper <junipertheory2@gmail.com>
2025-01-30 20:29:41 -0800

    first commit with the changes i had made on my end

--- main.lua
+++ main.lua
@@ -6,12 +6,23 @@ SMODS.Atlas {
   py = 34
 }
 
+mod_dir = ''..SMODS.current_mod.path
+ocConfig = SMODS.current_mod.config
+
 -- Load Decks
 local decks, load_error = SMODS.load_file("src/decks.lua")
 if load_error or decks == nil then
   sendDebugMessage("The error is: "..load_error)
 else
   decks()
+end
+
+-- Load Jokers
+local ocJokers, load_error = SMODS.load_file("src/jokers.lua")
+if load_error or ocJokers == nil then
+  sendDebugMessage("The error is: "..load_error)
+else
+  ocJokers()
 end
 
 -- REQUIRES: Malverk. Load replacement textures.
