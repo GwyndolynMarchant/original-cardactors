309fd3e8f0d7b10b44dea0049de68dd8b981313d
junipertheory <junipertheory2@gmail.com>
2025-02-18 21:53:26 -0500

    All the rezzies are now real

--- src/jokers.lua
+++ src/jokers.lua
@@ -130,7 +130,7 @@ SMODS.Joker{
         return false
     end,
     calculate = function(self, card, context)
-        if context.end_of_round == true and game_over == false then
+        if context.end_of_round and not context.repetition and not context.individual then
             G.E_MANAGER:add_event(Event({
                 func = function()
                     play_sound('tarot1')
@@ -146,17 +146,32 @@ SMODS.Joker{
                             G.jokers:remove_card(card)
                             card:remove()
                             card = nil
-                            G.E_MANAGER:add_event(Event({
-                                func = function()
-                                    -- We can just use SMODS.add_card to handle emplacement etc for us.
-                                    SMODS.add_card({
-                                        set = "Joker",
-                                        area = G.jokers,
-                                        key = 'j_ocs_rezzychips'
-                                    })
-                                    return true
-                                end
-                            }))
+                            return true
+                        end
+                    }))
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        delay = 0.3,
+                        blockable = false,
+                        func = function()
+                            -- I'm sorry for how this works but i'm very high right now and didn't want to figure it out better then this
+                            local rezzyRando = pseudorandom(pseudoseed("rezzytail"), 1 , 300)
+                            local rezzyChoice = 'j_ocs_rezzychips'
+                            if rezzyRando > 298 then
+                                rezzyChoice = 'j_ocs_rezzylegend'
+                            elseif rezzyRando > 280 then
+                                rezzyChoice = 'j_ocs_rezzyx'
+                            elseif rezzyRando > 200 then
+                                rezzyChoice =  'j_ocs_rezzycash'
+                            elseif rezzyRando > 100 then
+                                rezzyChoice = 'j_ocs_rezzymult'
+                            end
+                            _card = SMODS.add_card({
+                                set = "Joker",
+                                area = G.jokers,
+                                key = rezzyChoice
+                            })
+                            _card.ability.extra.rezzymainval = pseudorandom(pseudoseed("rezzytail"), _card.ability.extra.rezzyminval, _card.ability.extra.rezzymaxval)
                             return true
                         end
                     }))
@@ -173,7 +188,7 @@ SMODS.Joker{
     rarity = 1,
     atlas = 'rezzies',
     blueprint_compat = true,
-    config = {extra = {rezzymainval = 1,rezzyminval = 1, rezzymaxval = 100}},
+    config = {extra = {rezzymainval = 10,rezzyminval = 10, rezzymaxval = 100}},
     pos = {x = 0, y = 0},
     cost = 3,
     loc_txt = {
@@ -213,6 +228,207 @@ SMODS.Joker{
         if context.joker_main then
             return {
                 chips = card.ability.extra.rezzymainval
+            }
+        end
+    end
+}
+
+SMODS.Joker{
+    key = "rezzymult",
+    rarity = 1,
+    atlas = 'rezzies',
+    blueprint_compat = true,
+    config = {extra = {rezzymainval = 4,rezzyminval = 4, rezzymaxval = 20}},
+    pos = {x = 2, y = 0},
+    cost = 3,
+    loc_txt = {
+      name = 'Rhythm Rezzy',
+      text = {
+        [1] = "{C:mult}+#1#{} mult.",
+        [2] = "Generates a Tail when ",
+        [3] = 'destroyed or sold'
+      }
+    },
+    remove_from_deck = function(self, card, from_debuff)
+        if not from_debuff then
+            G.E_MANAGER:add_event(Event({
+                func = function() 
+                    SMODS.add_card({
+                        set = "Joker",
+                        area = G.jokers,
+                        key = 'j_ocs_rezzytail'
+                    })
+                    return true 
+                end
+            }))
+        end
+    end,
+    in_pool = function(self)
+        if next(SMODS.find_card("showman")) then
+            return true
+        elseif next(SMODS.find_card("rezzychips")) or next(SMODS.find_card("rezzymult")) or next(SMODS.find_card("rezzycash")) or next(SMODS.find_card("rezzyx")) or next(SMODS.find_card("rezzylegend")) then
+            return false
+        end
+        return true
+    end,
+    loc_vars = function(self, info_queue, card)
+      return { vars = {card.ability.extra.rezzymainval} }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then
+            return {
+                mult = card.ability.extra.rezzymainval
+            }
+        end
+    end
+}
+
+SMODS.Joker{
+    key = "rezzycash",
+    rarity = 2,
+    atlas = 'rezzies',
+    blueprint_compat = true,
+    config = {extra = {rezzymainval = 2,rezzyminval = 2, rezzymaxval = 10}},
+    pos = {x = 0, y = 1},
+    cost = 3,
+    loc_txt = {
+      name = 'Ritzy Rezzie',
+      text = {
+        [1] = "Earn {C:money}+#1#{} at",
+        [2] = "end of round.",
+        [3] = "Generates a Tail when ",
+        [4] = 'destroyed or sold'
+      }
+    },
+    remove_from_deck = function(self, card, from_debuff)
+        if not from_debuff then
+            G.E_MANAGER:add_event(Event({
+                func = function() 
+                    SMODS.add_card({
+                        set = "Joker",
+                        area = G.jokers,
+                        key = 'j_ocs_rezzytail'
+                    })
+                    return true 
+                end
+            }))
+        end
+    end,
+    in_pool = function(self)
+        if next(SMODS.find_card("showman")) then
+            return true
+        elseif next(SMODS.find_card("rezzychips")) or next(SMODS.find_card("rezzymult")) or next(SMODS.find_card("rezzycash")) or next(SMODS.find_card("rezzyx")) or next(SMODS.find_card("rezzylegend")) then
+            return false
+        end
+        return true
+    end,
+    loc_vars = function(self, info_queue, card)
+      return { vars = {card.ability.extra.rezzymainval} }
+    end,
+    calc_dollar_bonus = function(self,card)
+        local bonus = card.ability.extra.rezzymainval
+        if bonus > 0 then return bonus end
+    end
+}
+
+SMODS.Joker{
+    key = "rezzyx",
+    rarity = 3,
+    atlas = 'rezzies',
+    blueprint_compat = true,
+    config = {extra = {rezzymainval = 11,rezzyminval = 11, rezzymaxval = 35}},
+    pos = {x = 1, y = 0},
+    cost = 3,
+    loc_txt = {
+      name = 'X Rated Rezzy',
+      text = {
+        [1] = "{X:mult,C:white} X#1# {} Mult",
+        [2] = "Generates a Tail when ",
+        [3] = 'destroyed or sold'
+      }
+    },
+    remove_from_deck = function(self, card, from_debuff)
+        if not from_debuff then
+            G.E_MANAGER:add_event(Event({
+                func = function() 
+                    SMODS.add_card({
+                        set = "Joker",
+                        area = G.jokers,
+                        key = 'j_ocs_rezzytail'
+                    })
+                    return true 
+                end
+            }))
+        end
+    end,
+    in_pool = function(self)
+        if next(SMODS.find_card("showman")) then
+            return true
+        elseif next(SMODS.find_card("rezzychips")) or next(SMODS.find_card("rezzymult")) or next(SMODS.find_card("rezzycash")) or next(SMODS.find_card("rezzyx")) or next(SMODS.find_card("rezzylegend")) then
+            return false
+        end
+        return true
+    end,
+    loc_vars = function(self, info_queue, card)
+      return { vars = {card.ability.extra.rezzymainval * 0.1} }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then
+            return {
+                xmult = card.ability.extra.rezzymainval * .1
+            }
+        end
+    end
+}
+
+SMODS.Joker{
+    key = "rezzylegend",
+    rarity = 4,
+    atlas = 'rezzies',
+    blueprint_compat = true,
+    config = {extra = {rezzymainval = 1,rezzyminval = 1, rezzymaxval = 3}},
+    pos = {x = 3, y = 0},
+    cost = 3,
+    soul_pos = {x = 3, y = 1 },
+    loc_txt = {
+      name = 'Rezzy of Legend',
+      text = {
+        [1] = "Retrigger all played cards {C:chips}+#1#{} times.",
+        [2] = "Generates a Tail when ",
+        [3] = 'destroyed or sold'
+      }
+    },
+    remove_from_deck = function(self, card, from_debuff)
+        if not from_debuff then
+            G.E_MANAGER:add_event(Event({
+                func = function() 
+                    SMODS.add_card({
+                        set = "Joker",
+                        area = G.jokers,
+                        key = 'j_ocs_rezzytail'
+                    })
+                    return true 
+                end
+            }))
+        end
+    end,
+    in_pool = function(self)
+        if next(SMODS.find_card("showman")) then
+            return true
+        elseif next(SMODS.find_card("rezzychips")) or next(SMODS.find_card("rezzymult")) or next(SMODS.find_card("rezzycash")) or next(SMODS.find_card("rezzyx")) or next(SMODS.find_card("rezzylegend")) then
+            return false
+        end
+        return true
+    end,
+    loc_vars = function(self, info_queue, card)
+      return { vars = {card.ability.extra.rezzymainval} }
+    end,
+    calculate = function(self, card, context)
+        if context.cardarea == G.play and context.repetition and not context.repetition_only then
+            return {
+                message = 'Again!',
+                repetitions = card.ability.extra.rezzymainval,
+                card = context.other_card
             }
         end
     end
